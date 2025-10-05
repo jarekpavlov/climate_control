290da105267f8a0830b4a54be81353d4e2d0c846
jarek <iaroslav.pavlov@unibas.ch>
2025-10-05 09:28:55 +0200

    Revert "using only wire"
    This reverts commit 83e5824ccf344bd1f23961be69e24fae4acfc934.

--- sketch_dec10a.ino
+++ sketch_dec10a.ino
@@ -1,8 +1,6 @@
 #define but_1 11                      // button 1 / Pin D11
 #define but_2 12                      // button 2 / Pin D12
 
-float humidity;
-float temperature;
 byte sensorPin = 0;
 byte lightSensorPin = 2;		  
 int sensorValue = 0;  
@@ -19,13 +17,12 @@ unsigned long pauseTime = 0;
 bool pumpIsOn = false;
 bool lampIsOn = false;
 int currentButtonRegime = 0;
-int timeToCheck = 0;
 
 #include <avr/wdt.h>
+#include <AHT20.h>
 #include<math.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Wire.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -33,22 +30,11 @@ int timeToCheck = 0;
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define AHT10_ADDRESS 0x38
+AHT20 aht20;
+
 
 void setup() {
   Serial.begin(9600);
-  Wire.begin();
-  delay(100); // Allow sensor to boot
-
-  // Initialize AHT10
-  Wire.beginTransmission(AHT10_ADDRESS);
-  Wire.write(0xE1); // Initialization command
-  Wire.write(0x08); // Normal mode
-  Wire.write(0x00); // Calibration enable
-  Wire.endTransmission();
-  
-  delay(500); // Wait for sensor to calibrate
-
   pinMode(pumpOut, OUTPUT);
   pinMode(lampOut, OUTPUT);
   pinMode(but_1, INPUT_PULLUP); 
@@ -61,14 +47,17 @@ void setup() {
   }
   // Clear the buffer
   display.clearDisplay();
+
+  if (aht20.begin() == false)
+  {
+    Serial.println("AHT20 not detected. Please check wiring. Freezing.");
+    while (1);
+  }
   wdt_enable(WDTO_8S);
 }
 
 
 void loop() {
-
-  getSensorDataAndUpdateScreen();
-
   if (lampIsOn) {
     digitalWrite(lampOut, HIGH);
     if (analogRead(lightSensorPin) > 750) {
@@ -112,63 +101,23 @@ void loop() {
     //EEPROM.write(1,rollTrimMiddle/4); 
   }   
 
-  if ((humidity < minHumidity) && !pumpIsOn && (pauseTime + delayPausePump) < millis()) {
+  if ((aht20.getHumidity() < minHumidity) && !pumpIsOn && (pauseTime + delayPausePump) < millis()) {
     pumpIsOn = true;
     startTime = millis();
     pauseTime = 0;
   }
-  wdt_reset();
-}
 
-void getSensorDataAndUpdateScreen() {
-// Trigger measurement
-  Wire.beginTransmission(AHT10_ADDRESS);
-  Wire.write(0xAC); // Trigger measurement command
-  Wire.write(0x33); // Typical trigger values
-  Wire.write(0x00);
-  Wire.endTransmission();
-
-  delay(80); // Wait for measurement to complete (typical ~75ms)
-
-    // Request 6 bytes of data
-  Wire.requestFrom(AHT10_ADDRESS, 6);
-  if (Wire.available() == 6) {
-    byte data[6];
-    for (int i = 0; i < 6; i++) {
-      data[i] = Wire.read();
-    }
-
-    // Check status bit (bit 7 of first byte)
-    if ((data[0] & 0x80) == 0) {
-      // Combine bytes into raw values
-      uint32_t rawHumidity = ((uint32_t)(data[1]) << 12) |
-                             ((uint32_t)(data[2]) << 4) |
-                             ((data[3] & 0xF0) >> 4);
-                             
-      uint32_t rawTemperature = ((uint32_t)(data[3] & 0x0F) << 16) |
-                                ((uint32_t)(data[4]) << 8) |
-                                (uint32_t)(data[5]);
-
-      // Convert to physical values
-      humidity = ((float)rawHumidity / 1048576.0) * 100.0;
-      temperature = ((float)rawTemperature / 1048576.0) * 200.0 - 50.0;
-    } else {
-      Serial.println("Sensor is busy or not ready.");
-    }
-  } else {
-    Serial.println("Failed to read data from AHT10");
-  }
   display.clearDisplay();
   display.setTextSize(1); 
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,0);
   display.print(F("Hum./Min, %: "));
-  display.print(round(humidity));
+  display.print(round(aht20.getHumidity()));
   display.print("/");
   display.print(minHumidity);
   display.setCursor(0,10);
   display.print(F("Temp., C:     "));
-  display.print(round(temperature));
+  display.print(round(aht20.getTemperature()));
   display.display();
-  delay(200);
+  wdt_reset();
 }
