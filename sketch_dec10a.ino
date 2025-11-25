67f8530f9f2db5ba7e3704d855716c5a18b4aa16
jarek <iaroslav.pavlov@unibas.ch>
2025-11-25 18:22:05 +0100

    making sensor sencibility space for turning of the lamp after it was turn on

--- sketch_dec10a.ino
+++ sketch_dec10a.ino
@@ -68,7 +68,7 @@ void loop() {
 
   if (lampIsOn) {
     digitalWrite(lampOut, HIGH);
-    if (analogRead(lightSensorPin) > 750) {
+    if (analogRead(lightSensorPin) > 760) {
       lampIsOn = false;
       digitalWrite(lampOut, LOW);
       lampPauseTime = millis();
@@ -76,7 +76,7 @@ void loop() {
   } else {
     digitalWrite(lampOut, LOW);
   }
-  if (analogRead(lightSensorPin) < 750 && (lampPauseTime + delayPauseLamp) < millis()) {
+  if (analogRead(lightSensorPin) < 740 && (lampPauseTime + delayPauseLamp) < millis()) {
     lampIsOn = true;
   }
 
